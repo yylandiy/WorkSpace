a95045e29c33e18292f0a80758eb8ba68ee47361
Lanidy <33409329+yylandiy@users.noreply.github.com>
2018-06-05 17:30:23 +0800

    Lanidy
    第一次测试

---
+++ TestGit/src/landiy/Test/TestOne.java
@@ -0,0 +1,10 @@
+package landiy.Test;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Test - GitHub");
+	}
+
+}
